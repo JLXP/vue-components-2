9a162bc3bbbac93b0136c3677fb3730ad424ff48
JavierXool <javier.xool@outlook.com>
2025-11-24 21:57:29 -0600

    Configuracion mas limpia para components

--- src/components/ContactUs.vue
+++ src/components/ContactUs.vue
@@ -1,6 +1,18 @@
 <template>
-    <div class="">
-        <h1>Contact Us</h1>
-        <p>Please send and email to hello@dotnetmastery.com</p>
-    </div>
+  <div class="">
+    <h1>Contact Us</h1>
+    <p>Please send and email to {{ email }}</p>
+  </div>
 </template>
+<script>
+import { ref } from "vue";
+export default {
+  setup() {
+    const email = ref("hello@dotnetmastery.com");
+
+    return {
+      email,
+    };
+  },
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import ContactUs from "./components/ContactUs.vue";
+import ButtonContainer from "./components/ButtonContainer.vue";
 
 const app = createApp(App)
 
-app.component('contact-us', ContactUs)
+app.component('contact-us', ContactUs);
+app.component("button-counter", ButtonContainer)
 
 app.mount('#app');
