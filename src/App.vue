09071b2b47886a274d842c0cf95526c5501d6558
JavierXool <javier.xool@outlook.com>
2025-11-24 22:50:21 -0600

    Agregar Bootstrap a  vue

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
-  <div>{{ message }}</div>
-  <ContactUs></ContactUs>
-  <!--local component-->
-  <ButtonContainer></ButtonContainer>
+  <div class="container">
+    <div>{{ message }}</div>
+    <ContactUs></ContactUs>
+    <!--local component-->
+    <ButtonContainer></ButtonContainer>
+  </div>
 </template>
 
 <script setup>
-import ContactUs from './components/ContactUs.vue';
-import ButtonContainer from './components/ButtonContainer.vue';
+import ContactUs from "./components/ContactUs.vue";
+import ButtonContainer from "./components/ButtonContainer.vue";
 const message = "Hello vue";
 </script>
 
