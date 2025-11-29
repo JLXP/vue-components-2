a7c7f4ccfaefa11962787f623823edb3448223dc
JavierXool <javier.xool@outlook.com>
2025-11-28 23:58:43 -0600

    new component

---
+++ src/components/AddContact.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="container border rounded p-2">
+    <div class="text-center text-white">Add New Contact</div>
+    <form @submit.prevent="addContact">
+      <div class="row">
+        <div class="col-4">
+          <input
+            type="text"
+            class="form-control"
+            v-model="contact.name"
+            placeholder="Name"
+          />
+        </div>
+        <div class="col-4">
+          <input
+            type="text"
+            class="form-control"
+            v-model="contact.name"
+            placeholder="Email"
+          />
+        </div>
+        <div class="col-4">
+          <input
+            type="text"
+            class="form-control"
+            v-model="contact.name"
+            placeholder="Phone"
+          />
+        </div>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+import { reactive } from "vue";
+
+const contact = reactive({
+  name: "",
+  phone: "",
+  email: "",
+});
+
+function addContact() {
+  console.log(contact);
+  contact.email = "";
+  contact.name = "";
+  contact.phone = "";
+}
+s;
+</script>
