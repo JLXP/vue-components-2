13c7fa97b51e062f92acf31a130d6c3955f1a086
JavierXool <javier.xool@outlook.com>
2025-12-08 10:00:15 -0600

    Compononentes dinamicos en Vue

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,30 @@
   <div class="bg-black text pt-3" :style="{ height: '100vh' }">
     <h1 class="text-center text-success">ContactOPedia</h1>
     <div class="container">
-      <LuckyNumber></LuckyNumber>
-      <br />
-      <LuckyNumber2></LuckyNumber2>
+      <button class="btn btn-primary text-black m-2" @click="newVersion = !newVersion">Toggle Component</button>
+      <br/>
+
+      <button class="btn btn-primary text-black m-2" @click="newVersion=false">Lucky Number V1</button>
+      <button class="btn btn-primary text-black m-2" @click="newVersion=true">Lucky Number V2</button>
+      <br/>
+      <br/>
+      <component :is="currentComponent" />
     </div>
   </div>
 </template>
 
 <script setup>
-import { reactive, ref, provide } from "vue";
+import { computed, ref } from "vue";
 import LuckyNumber from "./components/LuckyNumber.vue";
 import LuckyNumber2 from "./components/LuckyNumber2.vue";
+
+const newVersion = ref(false);
+
+const currentComponent = computed(()=>{
+  return newVersion.value ? LuckyNumber2 : LuckyNumber;
+})
+
+
 </script>
 
 <style></style>
