a3a28cf3944fad0363c216100264ae206468d6bc
JavierXool <javier.xool@outlook.com>
2025-12-08 09:39:00 -0600

    Limpieza y Generador de Numersos de la suerte V2

--- src/App.vue
+++ src/App.vue
@@ -2,79 +2,17 @@
   <div class="bg-black text pt-3" :style="{ height: '100vh' }">
     <h1 class="text-center text-success">ContactOPedia</h1>
     <div class="container">
-      <div class="row text-white p-2 mb-2">
-        <div class="col-6">Owner Name: <input v-model="ownerName" /></div>
-        <div class="col-6 text-end">
-          Max Lucky Number : <input v-model.number="maxNumber" />
-        </div>
-      </div>
+      <LuckyNumber></LuckyNumber>
       <br />
-      <br />
-      <AddContact :onAddcontact="onAddContact"></AddContact>
-      <div class="row">
-        <div class="col-12" v-for="contact in contacts" :key="contact.name">
-          <Contact
-            :name="contact.name"
-            :phone="contact.phone"
-            :ownername="contact.ownerName"
-            :email="contact.email"
-            :isFavorite="contact.isFavorite"
-            @update-favorite="
-              contact.isFavorite = onUpdateFavorite($event, contact.phone)
-            "
-          >
-          </Contact>
-        </div>
-      </div>
-      <!--<LuckyNumber :maxNumber="10"></LuckyNumber>-->
-      <!--local component-->
-      <!-- <ButtonContainer></ButtonContainer> -->
+      <LuckyNumber2></LuckyNumber2>
     </div>
   </div>
 </template>
 
 <script setup>
 import { reactive, ref, provide } from "vue";
-import Contact from "./components/Contact.vue";
-import AddContact from "./components/AddContact.vue";
 import LuckyNumber from "./components/LuckyNumber.vue";
-//import ButtonContainer from "./components/ButtonContainer.vue";
-const maxNumber = ref(100);
-const ownerName = ref("dotnetmastery");
-provide("maxLuckyNumber", maxNumber);
-const contacts = reactive([
-  {
-    name: "Bruhgen",
-    phone: 123131231,
-    ownerName: ownerName,
-    isFavorite: false,
-  },
-  {
-    name: "Bella",
-    phone: 5554443333,
-    ownerName: ownerName,
-    isFavorite: true,
-  },
-  {
-    name: "Ben",
-    phone: 33344455543,
-    ownerName: "",
-    email: "ben@dotnetmastery.com",
-    isFavorite: false,
-  },
-]);
-
-function onAddContact(contact) {
-  contact.ownerName = ownerName.value;
-  contact.isFavorite = false;
-  contacts.push(contact);
-}
-
-function onUpdateFavorite(oldValuesFromChildComponent, phoneNumberFromParent) {
-  console.log(oldValuesFromChildComponent);
-  console.log(phoneNumberFromParent);
-  return !oldValuesFromChildComponent.isFavorite;
-}
+import LuckyNumber2 from "./components/LuckyNumber2.vue";
 </script>
 
 <style></style>
