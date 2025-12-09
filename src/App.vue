97c930b7400a1fcb1b12b97d6853623bc136d2d2
JavierXool <javier.xool@outlook.com>
2025-12-08 18:12:56 -0600

    Pasos de limpieza del ciclo de vida

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,10 @@
 <template>
-  <div class="bg-black text pt-3" :style="{ height: '100vh' }">
-    <h1 class="text-center text-success">ContactOPedia</h1>
-    <div class="container">
-      <button class="btn btn-primary text-black m-2" @click="newVersion = !newVersion">Toggle Component</button>
-      <br/>
+<LuckyNumberParentComponent />
 
-      <button class="btn btn-primary text-black m-2" @click="newVersion=false">Lucky Number V1</button>
-      <button class="btn btn-primary text-black m-2" @click="newVersion=true">Lucky Number V2</button>
-      <br/>
-      <br/>
-      <keep-alive>
-        <component :is="currentComponent" />
-      </keep-alive>   
-    </div>
-  </div>
+
 </template>
 
 <script setup>
-import { computed, ref } from "vue";
-import LuckyNumber from "./components/LuckyNumber.vue";
-import LuckyNumber2 from "./components/LuckyNumber2.vue";
-
-const newVersion = ref(false);
-
-const currentComponent = computed(()=>{
-  return newVersion.value ? LuckyNumber2 : LuckyNumber;
-})
-
+import LuckyNumberParentComponent from './components/LuckyNumberParentComponent.vue';
 
 </script>
-
-<style></style>
---
+++ src/components/LuckyNumberParentComponent.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="bg-black text pt-3" :style="{ height: '100vh' }">
+    <h1 class="text-center text-success">ContactOPedia</h1>
+    <div class="container">
+      <button class="btn btn-primary text-black m-2" @click="newVersion = !newVersion">Toggle Component</button>
+      <br/>
+
+      <button class="btn btn-primary text-black m-2" @click="newVersion=false">Lucky Number V1</button>
+      <button class="btn btn-primary text-black m-2" @click="newVersion=true">Lucky Number V2</button>
+      <br/>
+      <br/>
+      <keep-alive :include="['LuckyNumber','LuckyNumber2']">
+        <component :is="currentComponent" />
+      </keep-alive>   
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { computed, ref } from "vue";
+import LuckyNumber from "./LuckyNumber.vue";
+import LuckyNumber2 from "./LuckyNumber2.vue";
+
+const newVersion = ref(false);
+
+const currentComponent = computed(()=>{
+  return newVersion.value ? LuckyNumber2 : LuckyNumber;
+})
+
+
+</script>
+
+<style></style>
