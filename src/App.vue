ae934417979f1f16e6e445dab3a4ff73ea9943e8
JavierXool <javier.xool@outlook.com>
2025-12-09 23:59:27 -0600

    lifecycle demo part 1

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,15 @@
 <template>
   <h1>Hello vue</h1>
+  <ButtonContainer/>
 </template>
 
 <script setup>
+import ButtonContainer from './components/ButtonContainer.vue';
+import { onBeforeMount, onMounted, onBeforeUpdate, onUpdated, onBeforeUnmount, onUnmounted  } from 'vue';
+
+
+
+
 </script>
 
 
