cbc38caa96aa335236c32d1887d4c18f7972a6be
JavierXool <javier.xool@outlook.com>
2025-12-11 14:55:42 -0600

    Composables in action

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
-  <h1>Hello World</h1>
+  <button @click="increment">Increment</button>
+  <button @click="decrement">Decrement</button>
+  <p>{{ count }}</p>
 </template>
 
 <script setup>
+import { useCounter } from "./composibles/useCounter";
 
+const { count, increment, decrement } = useCounter();
 </script>
