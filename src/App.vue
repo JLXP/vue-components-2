4e74c148d21d6218e8bc9a2d7e3f2ec195a416ad
JavierXool <javier.xool@outlook.com>
2025-12-11 14:29:54 -0600

    Limpieza del ProyectoFinal

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,7 @@
 <template>
-  <h1>Hello vue</h1>
-  <button @click="showMessage">Show Message</button>
-  <br />
-  <h1>{{ count }}</h1>
-  <button @click="count++">Clicked {{ count }} times</button>
-  <ButtonContainer v-if="count == 1" />
+  <h1>Hello World</h1>
 </template>
 
 <script setup>
-import ButtonContainer from "./components/ButtonContainer.vue";
-import {
-  onBeforeMount,
-  onMounted,
-  onBeforeUpdate,
-  onUpdated,
-  onBeforeUnmount,
-  onUnmounted,
-  ref,
-} from "vue";
 
-const message = ref("Hello Vue");
-const showMessage = () => {
-  message.value = "Hello Vue from function";
-};
-const count = ref(0);
-
-onBeforeMount(() => {
-  console.log("onBeforeMount - App.vue");
-});
-
-onMounted(() => {
-  console.log("onMounted - App.vue");
-});
-
-onBeforeUpdate(() => {
-  console.log("onBeforeUpdate - App.vue");
-});
-
-onUpdated(() => {
-  console.log("onUpdated - App.vue");
-});
-
-onBeforeUnmount(() => {
-  console.log("onBeforeUnmount - App.vue");
-});
-
-onUnmounted(() => {
-  console.log("onUnmounted - App.vue");
-});
 </script>
--- src/components/ButtonContainer.vue
+++ src/components/ButtonContainer.vue
@@ -1,40 +1,6 @@
 <template>
-  <button @click="count++">Clicked {{ count }} times</button>
+  Counter
 </template>
 <script setup>
-import {
-  ref,
-  onBeforeMount,
-  onMounted,
-  onBeforeUpdate,
-  onUpdated,
-  onBeforeUnmount,
-  onUnmounted,
-} from "vue";
 
-onBeforeMount(() => {
-  console.log("onBeforeMount - ButtonCounter.vue");
-});
-
-onMounted(() => {
-  console.log("onMounted - ButtonCounter.vue");
-});
-
-onBeforeUpdate(() => {
-  console.log("onBeforeUpdate - ButtonCounter.vue");
-});
-
-onUpdated(() => {
-  console.log("onUpdated - ButtonCounter.vue");
-});
-
-onBeforeUnmount(() => {
-  console.log("onBeforeUnmount - ButtonCounter.vue");
-});
-
-onUnmounted(() => {
-  console.log("onUnmounted - ButtonCounter.vue");
-});
-
-const count = ref(0);
 </script>
